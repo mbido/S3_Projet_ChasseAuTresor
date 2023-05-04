350e4951ed177f9d79013e9f804ef4d9a61800cc
juliengauthier410 <juliengauthier410@penguin>
2023-05-04 12:54:55 +0200

    add glue.java

---
+++ projet/ChasseAuTresor/src/modele/Glue.java
@@ -0,0 +1,16 @@
+package modele;
+
+public class Glue extends Occupant {
+
+    public Glue(Position position) {
+        super(position);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void process(Moveable m) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
